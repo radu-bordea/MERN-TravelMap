82c0c03aadf4ca234c76778bed9da47d873f79a9
radu-bordea <radu.bordea.dev@gmail.com>
2024-12-20 14:09:26 +0200

    adjust map size

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
-import ReactMapGl from "react-map-gl";
+import ReactMapGl, { Marker } from "react-map-gl";
+import PlaceIcon from "@mui/icons-material/Place";
 
 function App() {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
+    width: "100vw",
+    height: "100vw",
+    latitude: 36.34232,
+    longitude: 17.4376,
+    zoom: 4,
   });
 
   return (
@@ -16,7 +17,16 @@ function App() {
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
         onViewStateChange={({ viewState }) => setViewport(viewState)} // Use the correct handler
-      />
+      >
+        <Marker
+          latitude={48.858093}
+          longitude={2.295694}
+          offsetLeft={-20}
+          offsetTop={-10}
+        >
+          <PlaceIcon style={{fontSize:viewport.zoom * 7, color:"slateblue"}}/>
+        </Marker>
+      </ReactMapGl>
     </div>
   );
 }
