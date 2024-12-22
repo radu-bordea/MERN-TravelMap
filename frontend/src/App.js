b891fda3686848761dd6b214162f316208103856
radu-bordea <radu.bordea.dev@gmail.com>
2024-12-22 12:29:55 +0200

    create new place and submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,25 +5,63 @@ import StarIcon from "@mui/icons-material/Star";
 import axios from "axios";
 import { format } from "timeago.js";
 import "./app.css";
+import { width } from "@mui/system";
 
 function App() {
+  const currentUser = "John";
   const [pins, setPins] = useState([]);
   const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  const [newPlace, setNewPlace] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [rating, setRating] = useState(0);
 
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vw",
-    latitude: 36.34232,
-    longitude: 17.4376,
+    latitude: 47.040182,
+    longitude: 17.071727,
     zoom: 4,
   });
+
+  const handleMarkerClick = (id, lat, long) => {
+    setCurrentPlaceId(id);
+    setViewport({ ...viewport, latitude: lat, longitude: long });
+  };
+
+  const handleAddClick = (e) => {
+    const [longitude, latitude] = e.lngLat;
+    setNewPlace({
+      lat: latitude,
+      long: longitude,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPin = {
+      username: currentPlaceId,
+      title,
+      desc,
+      rating,
+      lat: newPlace.lat,
+      long: newPlace.long,
+    };
+
+    try {
+      const res = await axios.post("/pins", newPin);
+      setPins([...pins, res.data]);
+      setNewPlace(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
     const getPins = async () => {
       try {
-        const response = await axios.get("/pins");
-        console.log("Pins data:", response.data);
-        setPins(response.data);
+        const allPins = await axios.get("/pins");
+        setPins(allPins.data);
       } catch (err) {
         console.log(err);
       }
@@ -31,66 +69,119 @@ function App() {
     getPins();
   }, []);
 
-  const handleMarkerClick = (id) => {
-    setCurrentPlaceId(id);
-  };
-
   return (
-    <div className="App">
+    <div className={{ height: "100vw", width: "100%" }}>
       <ReactMapGl
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
         onViewStateChange={({ viewState }) => setViewport(viewState)} // Use the correct handler
+        onDblClick={handleAddClick}
+        transitionDuration="200"
       >
         {pins.map((p) => (
           <>
             <Marker
               latitude={p.lat}
               longitude={p.long}
-              offsetLeft={-20}
-              offsetTop={-10}
+              offsetLeft={-3 * viewport.zoom}
+              offsetTop={-7 * viewport.zoom}
             >
               <PlaceIcon
-                style={{ fontSize: viewport.zoom * 7, color: "slateblue" }}
-                onClick={() => handleMarkerClick(p._id)}
+                style={{
+                  fontSize: viewport.zoom * 7,
+                  color: p.username === currentUser ? "tomato" : "slateblue",
+                  cursor: "pointer",
+                }}
+                onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
               />
             </Marker>
-            {
-              (p._id === currentPlaceId && (
-                <Popup
-                  latitude={p.lat}
-                  longitude={p.long}
-                  closeButton={true}
-                  closeOnClick={false}
-                  anchor="left"
-                  onClose={()=> setCurrentPlaceId(null)}
-                >
-                  <div className="card">
-                    <label>Place</label>
-                    <h4 className="place">{p.title}</h4>
-                    <label>Review</label>
-                    <p className="desc">{p.desc}</p>
-                    <label>Rating</label>
-                    <label>
-                      <div className="stars">
-                        <StarIcon className="star" />
-                        <StarIcon className="star" />
-                        <StarIcon className="star" />
-                        <StarIcon className="star" />
-                        <StarIcon className="star" />
-                      </div>
-                    </label>
-                    <label>Information</label>
-                    <span className="username">
-                      Created by <b>{p.username}</b>
-                    </span>
-                    <span className="date">{format(p.createdAt)}</span>
-                  </div>
-                </Popup>
-              ))
-            }
+            {p._id === currentPlaceId && (
+              <Popup
+                latitude={p.lat}
+                longitude={p.long}
+                closeButton={true}
+                closeOnClick={false}
+                anchor="left"
+                onClose={() => setCurrentPlaceId(null)}
+                author="left"
+              >
+                <div className="card">
+                  <label>Place</label>
+                  <h4 className="place">{p.title}</h4>
+                  <label>Review</label>
+                  <p className="desc">{p.desc}</p>
+                  <label>Rating</label>
+                  <label>
+                    <div className="stars">
+                      <StarIcon className="star" />
+                      <StarIcon className="star" />
+                      <StarIcon className="star" />
+                      <StarIcon className="star" />
+                      <StarIcon className="star" />
+                    </div>
+                  </label>
+                  <label>Information</label>
+                  <span className="username">
+                    Created by <b>{p.username}</b>
+                  </span>
+                  <span className="date">{format(p.createdAt)}</span>
+                </div>
+              </Popup>
+            )}
           </>
         ))}
+        {newPlace && (
+          <>
+            <Marker
+              latitude={newPlace.lat}
+              longitude={newPlace.long}
+              offsetLeft={-3.5 * viewport.zoom}
+              offsetTop={-7 * viewport.zoom}
+            >
+              <PlaceIcon
+                style={{
+                  fontSize: 7 * viewport.zoom,
+                  color: "tomato",
+                  cursor: "pointer",
+                }}
+              />
+            </Marker>
+            <Popup
+              latitude={newPlace.lat}
+              longitude={newPlace.long}
+              closeButton={true}
+              closeOnClick={false}
+              anchor="left"
+              onClose={() => setNewPlace(null)}
+            >
+              <div>
+                <form onSubmit={handleSubmit}>
+                  <label>Title</label>
+                  <input
+                    placeholder="Enter a title"
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                  <label>Review</label>
+                  <textarea
+                    placeholder="Tell us something about this place."
+                    onChange={(e) => setDesc(e.target.value)}
+                  ></textarea>
+                  <label>Rating</label>
+                  <select onChange={(e) => setRating(e.target.value)}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </select>
+                  <button className="submitButton" type="submit">
+                    Add Pin
+                  </button>
+                </form>
+              </div>
+            </Popup>
+          </>
+        )}
       </ReactMapGl>
     </div>
   );
